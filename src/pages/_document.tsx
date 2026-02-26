6dfb603940b9d85e98c4da841acb30decc94dfba
Juan Parra <parrahoyosjuanignacion@gmail.com>
2026-02-26 17:57:21 -0500

    web

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <title>Soluciones informaticas</title>
+      <Head > 
+      <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Head>
+      <title>Soluciones Informáticas Urabá | Cámaras, Redes y Soporte Técnico</title>
+      <meta
+      name="description"
+      content="Instalación de cámaras de seguridad, redes, soporte técnico y soluciones tecnológicas en Urabá, Apartadó, Carepa y Chigorodó."
+      />
+      </Head>
+      
       <body className="antialiased">
         <Main />
         <NextScript />
